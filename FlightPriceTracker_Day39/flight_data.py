db277db3e9ddc7e29977dd52eae492308add18a0
Kirsten-Nancy <korjnan@gmail.com>
2021-02-13 17:11:00 +0800

    added stopover city functionality

--- FlightPriceTracker_Day39/flight_data.py
+++ FlightPriceTracker_Day39/flight_data.py
@@ -1,7 +1,7 @@
 class FlightData:
     #This class is responsible for structuring the flight data
     def __init__(self, price, dept_city_name, dept_airport_iata_code, arr_city_name,
-                 arr_airport_iata_code, outbound_date, inbound_date, stop_overs=0,via_city=" "):
+                 arr_airport_iata_code, outbound_date, inbound_date, stop_overs=0,via_city=""):
         self.price = price
         self.dept_city_name = dept_city_name
         self.dept_airport_iata_code = dept_airport_iata_code
